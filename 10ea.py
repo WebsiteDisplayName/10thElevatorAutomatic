b22c6fc0a3ee9570748d93c3e048512d975082c0
WebsiteDisplayName <rowemg@bc.edu>
2022-06-24 02:35:17 -0500

    final

--- 10ea.py
+++ 10ea.py
@@ -9,8 +9,8 @@ from tkinter.filedialog import askdirectory
 
 
 # root = 10thElevatorMusic (work folder)
-root = askdirectory(title='Select Folder') # shows dialog box and return the root 
-
+# shows dialog box and return the root
+root = askdirectory(title='Select Folder')
 
 
 for subdir, dirs, files in os.walk(root):
@@ -18,7 +18,7 @@ for subdir, dirs, files in os.walk(root):
         configFilePath = root + "/" + dir
         with open(configFilePath + "/config.cpp", "w") as myfile:
             myfile.write('''
-            
+
 class CfgPatches
 {
 		class ''' + dir + '''
@@ -34,148 +34,32 @@ class CfgMusicClasses
 {
 		class ''' + dir + '''
 	{
-		displayName = "::::''' + dir + '''::::" 
+		displayName = "::::''' + dir + '''::::"
 	};
-};            
-            
+};
+
 class CfgMusic
 {''')
             # https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
             for subdir, dirs, files in os.walk(configFilePath):
-				counter = 1
-				for i in files:
-					if i.endswith(".ogg"):
-						audio = AudioSegment.from_file(configFilePath + "/" + i)
-						myfile.write('''
+                counter = 1
+                for i in files:
+                    if i.endswith(".ogg"):
+                        audio = AudioSegment.from_file(
+                            configFilePath + "/" + i)
+                        myfile.write('''
 
 		class ''' + dir[0:5] + '''_''' + str(counter) + '''
 	{
 		name = "''' + i[0:-4] + '''";
 		sound[] = {"''' + dir + '''\\''' + i + '''",1,1};
-		theme = "'''+ dir +'''";
+		theme = "''' + dir + '''";
 		duration = "''' + str(int(audio.duration_seconds)) + '''";
-		musicClass = "'''+ dir +'''";
+		musicClass = "''' + dir + '''";
 	};                           
 ''')
-						counter += 1
+                        counter += 1
 
-
-				myfile.write('''
+                myfile.write('''
 
 };''')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
